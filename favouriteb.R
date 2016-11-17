78e10b74af485897657137cb4c8efabc35e03749
eugene <nikshev81@gmail.com>
2016-11-17 08:20:44 +0200

    Add functions for TDM

--- favouriteb.R
+++ favouriteb.R
@@ -10,6 +10,7 @@ cleanCorpus<-function(corpus){
   corpus.tmp <- tm_map(corpus, removePunctuation)
   corpus.tmp <- tm_map(corpus.tmp, stripWhitespace)
   corpus.tmp <- tm_map(corpus.tmp, tolower)
+  corpus.tmp <- tm_map(corpus.tmp, PlainTextDocument)
   corpus.tmp <- tm_map(corpus.tmp, removeWords, stopwords("russian"))
   return (corpus.tmp)
 }
@@ -17,18 +18,28 @@ cleanCorpus<-function(corpus){
 #build TDM
 generateTDM<-function(candidate, path){
   s.dir <- sprintf("%s/%s", path, candidate)
-  s.cor <- Corpus(DirSource(directory = s.dir, encoding="UTF8"))
-  s.cor.cl <- clearCorpus(s.cor)
+  s.cor <- Corpus(DirSource(directory = s.dir, encoding="UTF-8"))
+  s.cor.cl <- cleanCorpus(s.cor)
   s.tdm <- TermDocumentMatrix(s.cor.cl)
   s.tdm <- removeSparseTerms(s.tdm, 0.7)
   result <- list(name = candidate, tdm = s.tdm)
   return (result)
 }
-tdm <- lapply(candidates, generateTDM, path = pathname)
+tdm <- lapply(doc_class, generateTDM, path = pathname)
 
 #attach name
+bindCandidateToTDM <- function(tdm) {
+  s.mat <- t(data.matrix(tdm['tdm']))
+  s.df  <- as.data.frame(s.mat, stringAsFactors = FALSE)
+  s.df  <- cbind(s.df, rep(tdm[["name"]],nrow(s.df)))
+  colnames(s.df)[ncol(s.df)] <-"targetCandidate"
+  return (s.df)
+}
+candTDM <- lapply(tdm, bindCandidateToTDM)
 
 #stack
+tdm.stack <- do.call(rbind.fill, candTDM)
+tdm.stack[is.na(tdm.stack)] <- 0
 
 #hold-out
 
